735fe22d215b858b1ce2b1eab4e6c7074c9e585d
mayank738059 <88849981+mayank738059@users.noreply.github.com>
2026-06-26 13:09:48 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int i,int n,vector<int> &dp){
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int ones=solve(i+1,n,dp);
+        int twos=solve(i+2,n,dp);
+        return dp[i]= ones+twos;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(0,n,dp);
+    }
+};
