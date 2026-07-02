68ab6fa6c572c71a9973e6b4fde22051805f8e6e
mayank738059 <88849981+mayank738059@users.noreply.github.com>
2026-07-02 11:52:46 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int jump(vector<int>& nums) {
+        int jumps = 0, currentEnd = 0, farthest = 0;
+
+        // Traverse through the array (excluding the last element)
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            // Update the farthest index that can be reached so far
+            farthest = max(farthest, i + nums[i]);
+
+            // When we reach the end of the current jump range
+            if (i == currentEnd) {
+                // We need to make a jump
+                jumps++;
+
+                // Move the current end to the farthest index we can reach
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+};
